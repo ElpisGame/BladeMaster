fab8c0b99a73d7b7e4b01ffe9bc2ef49a7ee285f
Li-Qing Wang <liqingnz@gmail.com>
2024-02-18 16:33:45 +0800

    add deployment script, update contract addresses

--- README.md
+++ README.md
@@ -6,8 +6,10 @@
 | ----------- | ------------------------------------------ |
 | USDT        | 0x7DE99fe2687827cC60d38f626549226F0a68269A |
 | NFT         | 0x6B8a176Ab8e37dF3542fb34030Be66229a1361da |
-| Vault       | 0x995d342eB8af17902d4981dC3d54d0E780D6751B |
-| Vault Owner | 0x03A65893283C70beC8AC0F6b515Fc40042ce2091 |
+| Vault Imp   | 0xA96fEA207Cf93cF12E4599e63d4E42E34999F5c6 |
+| Vault Proxy | 0x421Cf19EbC51924b8493a3c4c6746FD1fb8Bf2fd |
+| Vault Proxy Admin | 0x780e69A7e72A1a554e95A9B5CDe3a76584B0Ad28 |
+| Vault Admin | 0x03A65893283C70beC8AC0F6b515Fc40042ce2091 |
 
 ### Polygon mainnet
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,68 @@
 import { ethers } from "hardhat";
+import {
+    ElpisOriginAsset721A,
+    ElpisOriginAsset721A__factory,
+    ElpisOriginMarket,
+    ElpisOriginMarket__factory,
+    ElpisOriginVault,
+    ElpisOriginVault__factory,
+    ElpisOriginSubToken,
+    ElpisOriginSubToken__factory,
+    // TransparentUpgradeableProxy,
+    // TransparentUpgradeableProxy__factory,
+    ElpisOriginProxy__factory,
+    ProxyAdmin,
+    ProxyAdmin__factory,
+    ProxyTest__factory,
+} from "../typechain-types";
+
+const testAccount = "0x9Ac48F8C16eB094B9432aE7FdDa7002Ef611d096";
+const ELPIS_ASSET_ADDRESS = "0x6B8a176Ab8e37dF3542fb34030Be66229a1361da";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const [deployer] = await ethers.getSigners();
+    const deployerAddr = await deployer.getAddress();
+    console.log(deployerAddr);
 
-  const lockedAmount = ethers.parseEther("0.001");
+    let vaultImplementation = await new ElpisOriginVault__factory(
+        deployer
+    ).deploy();
+    let vaultImpAddress = await vaultImplementation.getAddress();
+    await vaultImplementation.waitForDeployment();
+    console.log("Vault implementation address: ", vaultImpAddress);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+    let proxy = await new ElpisOriginProxy__factory(deployer).deploy(
+        vaultImpAddress,
+        deployerAddr,
+        "0x"
+    );
+    await proxy.waitForDeployment();
 
-  await lock.waitForDeployment();
+    let vaultProxyAddress = await proxy.getAddress();
+    let vaultProxyAdminAddress = await proxy.proxyAdmin();
+    console.log("Vault proxy address: ", vaultProxyAddress);
+    console.log("Vault proxy admin address: ", vaultProxyAdminAddress);
+    let vaultProxy = ElpisOriginVault__factory.connect(
+        vaultProxyAddress,
+        deployer
+    );
+    await vaultProxy.initialize(deployerAddr, deployerAddr);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    // let proxyAdmin = ProxyAdmin__factory.connect(
+    //     vaultProxyAdminAddress,
+    //     deployer
+    // );
+
+    let elpisAsset = ElpisOriginAsset721A__factory.connect(
+        ELPIS_ASSET_ADDRESS,
+        deployer
+    );
+    await elpisAsset.mintAssetBatch(vaultProxyAddress, 10);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
